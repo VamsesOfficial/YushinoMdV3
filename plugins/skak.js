3ae25e606d4824a1a163d48809e4bc5412494092
Vamses <108464869+VamsesOfficial@users.noreply.github.com>
2022-11-06 22:12:55 +0800

    Create skak.js

---
+++ plugins/skak.js
@@ -0,0 +1,29 @@
+import { sticker } from '../lib/sticker.js'
+import fs from 'fs'
+import path from 'path'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
+
+//m.reply(`Wait ${command} sedang proses🐦`)
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+let stiker = await sticker(null, global.API(`https://telegra.ph/file/d34b2ab2cb233c749776c.png`), global.packname, global.author)
+ conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, { fileLength: 100, contextInfo: {
+          externalAdReply :{
+          showAdAttribution: true,
+    mediaUrl: sgc,
+    mediaType: 2,
+    description: 'Slebew', 
+    title: `${command} Sedang Di Proses`,
+    body: botdate,
+    thumbnail: await(await fetch(pp)).buffer(),
+    sourceUrl: sgc
+     }}
+  })
+  }
+handler.command = /^(iya)$/i
+
+export default handler
